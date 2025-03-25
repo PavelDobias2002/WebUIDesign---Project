60a0722ae170ac1e81c26c8979fc60845b031efb
Pavel Dobias <dobias.parez@gmail.com>
2025-03-25 12:58:57 +0000

    starting making layout for News

--- svelte-app/src/lib/components/Navigation.svelte
+++ svelte-app/src/lib/components/Navigation.svelte
@@ -70,7 +70,7 @@
         margin-right: 4rem;
         gap: 4rem;
         display: flex;
-        background-color: black;
+        background-color: rgb(0, 0, 0);
         border-radius: 50px;
         justify-content: center;
         align-items: center;
@@ -84,7 +84,7 @@
         font-family: 'Pixelify Sans';
         font-size: large;
         text-decoration: none;
-        color: white;
+        color: rgb(255, 255, 255);
         font-weight: bold;
         display:inline-block;
         position:relative;
@@ -155,7 +155,7 @@
         position: absolute;
         left: 50%;
         transform: translateX(-50%) scale(0.35);
-        top:-150%;
+        top:-140%;
         z-index: 2;
     }
     
--- svelte-app/src/routes/news/+page.svelte
+++ svelte-app/src/routes/news/+page.svelte
@@ -4,56 +4,79 @@
 <div class="overlay"></div>
 <div class="header-container">
     <h1>News News and more News!</h1>
-    <h2>Mickey 17</h2>
+    <h2>From Cinema to your screen.</h2>
 </div>
 
-<div class="movie-container">
-	<div class="image">
-		<img src='/Mickey_17.jpg' alt="Mickey 17 movie poster">
-	</div>
-	<div class="title">
-		<h3>Mickey 17</h3>
-	</div>
-	<div class="genre">
-		<h3>genre:</h3>
-		<p>Sci-Fi/Comedy/Drama/Mystery & Thriller</p>
-	</div>
-	<div class="director">
-		<h3>director:</h3>
-		<p>Bong Joon Ho</p>
-	</div>
-	<div class="production">
-		<h3>production:</h3>
-		<p>	
-			Plan B Entertainment</p>
-			<p>Offscreen</p>
-			<p>Kate Street Picture Company<p>
-			<p>Domain Entertainment</p>
-	</div>
-	<div class="cast">
-		<h3>cast:</h3>
-		<p>Robert Pattinson</p>
-		<p>Mark Ruffalo</p>
-		<p>Toni Collette</p>
-		<p>Naomi Ackie</p>
-		<p>Steven Yeun</p>
-	</div>
-	<div class="cinemasIreland">
-		<h3>Now in theaters!</h3>
-		<p>Don't miss the opportunity now in any cinema in Ireland!</p>
-	</div>
+<div class="news-container">
+	
+    <div class="news-article">
 
+	    <div class="title">
+		    <h3>Nosferatu hunts your dreams</h3>
+	    </div>
+	    
+        <div class="paragraph">
+            <p>The new adaptaion of legendary Vampire is set to be an amazing watch for you and your family.</p>
 
-	<div class="description">
-		<h3>Description:</h3>
-		<p>Robert Pattinson, wide-eyed and squeaky-voiced, is Mickey Barnes, who needs to get off-Earth sharpish. In the absence of any discernible skill set, he signs up as an “expendable”, and to be killed over and over again on a colony ship headed to the planet of Niflheim under the authority of monstrous politician and cult leader Kenneth Marshall (Mark Ruffalo) and his wife Ylfa (Toni Collette). All is fine, apart from the dying and being recreated by a “human printer”, and Mickey even strikes up a relationship with security officer Nasha (Naomi Ackie). But then, Mickey no. 17 unexpectedly survives a near-death experience, and gets home to find the new version of himself, Mickey 18, already up and about. It’s a scenario that means death for both, so Mickey must finally develop some wits and try and keep them about him.</p>
-	</div>
-	<div class="trailer">
-		<h3>trailer</h3>
-		<iframe src="https://www.youtube.com/embed/osYpGSz_0i4?si=raKvz4ESw3Wj_jml"></iframe>
-	</div>
+        </div>
+
+        <div class="date">
+		    <h3>Date:</h3>
+		    <p>25/03/2025</p>
+	    </div>
+        
+        <div class="container">
+
+        </div>
+    </div>
+    
+    <div class="news-article">
+
+	    <div class="title">
+		    <h3>Mickey 17 made a splash at the cinemas</h3>
+	    </div>
+	    
+        <div class="paragraph">
+            <p>​Bong Joon Ho's latest film, "Mickey 17," is a sci-fi satire starring Robert Pattinson as Mickey Barnes, an expendable crew member on a space colonization mission. In this dystopian future, Earth is plagued by environmental disasters, prompting a mission to colonize Niflheim. Mickey's role involves repeated dangerous tasks, resulting in his death and subsequent reprinting with retained memories.</p>
+
+        </div>
+
+        <div class="date">
+		    <h3>Date:</h3>
+		    <p>25/03/2025</p>
+	    </div>
+        
+        <div class="container">
+
+        </div>
+    </div>
+
+    <div class="news-article">
+
+	    <div class="title">
+		    <h3>Mickey 17</h3>
+	    </div>
+	    
+        <div class="paragraph">
+            <p>​Bong Joon Ho's latest film, "Mickey 17," is a sci-fi satire starring Robert Pattinson as Mickey Barnes, an expendable crew member on a space colonization mission. In this dystopian future, Earth is plagued by environmental disasters, prompting a mission to colonize Niflheim. Mickey's role involves repeated dangerous tasks, resulting in his death and subsequent reprinting with retained memories. </p>
+
+        </div>
+
+        <div class="date">
+		    <h3>Date:</h3>
+		    <p>25/03/2025</p>
+	    </div>
+        
+        <div class="container">
+
+        </div>
+    </div>
+
+    
 	
 </div>
+
+
 
 <style>
 	    .overlay {
@@ -69,39 +92,43 @@
         z-index: -1;
     }
 
-	.movie-container .title h3{
+    .header-container h2{
+        font-family: "Exo 2", sans-serif;
+        font-optical-sizing: auto;
+        font-weight: 500;
+        font-style: normal;
+    }
+
+	.news-container h3{
 		font-family: "Exo 2", sans-serif;
         font-optical-sizing: auto;
         font-weight: 700;
         font-style: normal;
-        font-size: 3.5rem;
+        font-size: 2rem;
 		color:black;
 		letter-spacing: 2px;
 		text-transform: uppercase;
 	}
 	
-    .movie-container h3 {
-        font-size: 2rem;
-        color: #252525;
-        text-transform: capitalize;
-    }
-	.movie-container p{
+	.news-container p{
 		font-family: "Exo 2", sans-serif;
         font-optical-sizing: auto;
         font-weight: 500;
         font-style: normal;
+
+        
 	}
-	.movie-container > div{
+	.news-container > div{
 		background-color: rgba(154, 154, 154, 0.6);
 		border-radius:30px;
 		padding:1rem;
 	}
-    .movie-container{
+    .news-container{
         display: grid;
-        grid-template-columns: repeat(5, 1fr);
-        grid-template-rows: auto auto auto auto ;
-        gap: 30px;
-		padding:20px;
+        grid-template-columns: repeat(1, 1fr);
+        grid-template-rows: auto auto auto;
+        gap: 15px;
+		padding:10px;
         width:80%;
         margin:auto;
 		background-color: rgba(86, 94, 110, 0.6);
@@ -109,54 +136,57 @@
 		box-shadow: 0 0 15px rgba(255, 255, 255, 0.3);
     }
 
+    .news-article{
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        grid-template-rows: auto;
+    }
+
+    .news-article::after{
+        content: "";
+            display: block;
+            width: 40%;
+            height: 2px;
+            border-radius: 50px;
+            margin: 0 auto 10px auto;
+            transition: width 0.3s ease-in-out;
+    }
+
+    .news-article::before {
+        content: attr(data-text);
+        position: absolute;
+        overflow:hidden;
+        left:0;
+        width:0;
+        white-space: nowrap;
+        transition:all 0.5s;
+    }
+
 .image {
-	grid-area: 1 / 1 / 4 / 3; /*row start column start/row end column end*/
+	grid-area: 1 / 1 / 1/ 1; /*row start column start/row end column end*/
 }
 .image img{
-	border-radius: 30px;
+	border-radius: 10px;
 	width:100%;
 	height:100%;
 	object-fit: cover;
 }
 .title {
-	grid-area: 1 / 3 / 1 / 6;
+	grid-area: 1 / 1 / 1 / 2;
 	
 }
-.genre {
-	grid-area: 2/3/3/4;
-	
+
+.paragraph{
+    grid-area: 1 / 2 / 1 / 2;
 }
-.director {
-	grid-area: 3/3/4/4;
-	height:50%;
+
+.date {
+	grid-area: 1 / 3 / 1 / 3;
 }
-.production {
-	grid-area: 3/4/4/5;
+
+.container{
+    grid-area: 1 / 1 / 1 / 3;
+    z-index: -1;
 }
-.cast {
-	grid-area: 2 / 5 / 4 / 6;
-	
-}
-.cast p{
-	margin-bottom:20px;
-}
-.cinemasIreland {
-	grid-area: 2/4/3/5;
-	
-}
-.description {
-	grid-area: 4 / 1 / 7 / 4;
-	
-}
-.trailer {
-	grid-area: 4 / 4 / 7 / 6;
-}
-.trailer iframe {
-        width: 100%;  /* Make the iframe take up the full width of the container */
-		height: 80%;
-        border: none;
-		border-radius: 30px;
-        object-fit: cover; /* Makes the video cover the entire iframe while maintaining aspect ratio */
-    }
 
 </style>
